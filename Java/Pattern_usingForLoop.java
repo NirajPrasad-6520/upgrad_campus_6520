4070d9eb43a041554cd18b6452fa5c6046f262e8
Niraj <nirajprasad4y18@gmail.com>
2024-09-08 04:51:20 +0530

    first commit

--- Java/Pattern_usingForLoop.java
+++ Java/Pattern_usingForLoop.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 
 public class Pattern_usingForLoop {
-    public static void main(String []args){
+    public static void main(String []args)
+    {
+        //Right Triangle Star Pattern
         int rows;
+        System.out.println("Right Triangle Star Pattern");
         System.out.println("Enter rows:");
         Scanner sc=new Scanner(System.in);
         rows=sc.nextInt();
@@ -12,6 +15,17 @@ public class Pattern_usingForLoop {
             {
                 System.out.print("*");
             }
+            System.out.println();
+        }
+
+        //Pyramid Star Pattern
+        System.out.println("Pyramid Star Pattern");
+        int row=6;
+        for(int i=0;i<row;i++){
+            for(int j=row-i-1;j>=1;j--)
+            System.out.print(" ");
+            for(int j = 0;j<=i;j++)
+            System.out.print("* ");
             System.out.println();
         }
     }
