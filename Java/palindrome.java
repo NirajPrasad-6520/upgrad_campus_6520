11d486d2957b9776e23340d6540be99e340e52a7
Niraj <nirajprasad4y18@gmail.com>
2024-10-02 21:04:28 +0530

    first commit

---
+++ Java/palindrome.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class palindrome {
+    public static void main(String[]args)
+    {
+    //Palindrome Number
+        int n,c,r,s=0;
+        System.out.println("Enter any number :");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        c=n;
+        while(n>0)
+        {
+            r=n%10;
+            s=(s*10)+r;
+            n=n/10;
+        }
+        if(c==s)
+        System.out.println(c+ " is Palindrome Number");
+        else
+        System.out.println(c+ " is Not Palindrome Number beacuse it's reverse is "+s);
+        System.out.println();
+
+    //Palindrome Words
+        System.out.println("Enter Word");
+        String str=sc.next();
+        String rev="";
+
+        for(int i=0;i<str.length();i++)
+        rev=str.charAt(i)+rev;
+        if(str.equalsIgnoreCase(rev))
+        System.out.println(str+" is Palindrome Word");
+        else
+        System.out.println(str+" is Not Palindrome Word because it's reverse is "+rev);        
+    }
+}
