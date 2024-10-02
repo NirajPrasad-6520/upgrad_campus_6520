e251e3cf091350cc5405e437b8d09357da7b2d99
Niraj <nirajprasad4y18@gmail.com>
2024-10-03 02:05:38 +0530

    first commit

--- Java/ifelse.java
+++ Java/ifelse.java
@@ -5,30 +5,30 @@ public class ifelse {
         int number=10;
         if(number > 0)
         {
-            System.out.println("The number is greater than 0\n");
+            System.out.println("The number "+number+" is greater than 0.\n");
         }
         else if(number< 0)
         {
-            System.out.println("The number is less than 0\n");
+            System.out.println("The number"+number+" is less than 0.\n");
         }
         else 
         {
-            System.out.println("The number is 0\n");
+            System.out.println("The number is 0.\n");
         }
 
 
         //User Input code
         Scanner sc =new Scanner(System.in);
-        System.out.println("Enter number :");
+        System.out.println("Enter the number to check given number is Greater than 0 or Not :");
         int num =sc.nextInt();
         if(num>0){
-            System.out.println("The number is greater than 0\n");
+            System.out.println("The number is greater than 0.\n");
         }
         else if (num<0){
             System.out.println("The number is less than 0\n");
         }
         else {
-            System.out.println("The number is 0\n");
+            System.out.println("The number is 0.\n");
         }  
 
         //Even Or Odd 
