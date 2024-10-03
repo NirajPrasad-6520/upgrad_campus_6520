85e77535fc52b5c283501966149dcc3426c93d6c
Niraj <nirajprasad4y18@gmail.com>
2024-10-03 21:20:45 +0530

    first commit

---
+++ Java/randomnumber.java
@@ -0,0 +1,42 @@
+import java.lang.Math;
+import java.util.random.*;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom; 
+public class randomnumber {
+    
+    /**
+     * Generating Random Number in Java
+     * 1. Random Class
+     * 2. Math.Random()
+     * 3. ThreadLocalRandom
+     */
+
+     public static void main(String[]args)
+     {
+       Random  random=new Random();
+       System.out.println("1. Random class: \n");
+         for(int i=1;i<=5;i++)
+           {
+               System.out.println("Random int: "+ random.nextInt());
+               System.out.println("Random double: "+ random.nextDouble());
+               System.out.println("Random float: "+ random.nextFloat());
+               System.out.println("Random boolean: "+ random.nextBoolean()+"\n");
+           }
+
+           System.out.println("2. Math.random():\n");
+           for(int i=1;i<=5;i++)
+           {
+               System.out.println("1st Random Number: " + Math.random());
+               System.out.println("2nd Random Number: " + Math.random());
+               System.out.println("3rd Random Number: " + Math.random());
+               System.out.println("4th Random Number: " + Math.random()+"\n");
+           }   
+
+           System.out.println("3. ThreadLocalRandom");
+           System.out.println(ThreadLocalRandom.current().nextInt());
+           System.out.println(ThreadLocalRandom.current().nextDouble());
+           System.out.println(ThreadLocalRandom.current().nextFloat());
+           System.out.println(ThreadLocalRandom.current().nextBoolean());
+
+     }
+}
---
+++ Java/reversenumber.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class reversenumber {
+    public static void main(String[] args) {
+        int n,r;
+        System.out.println("Enter number:");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        while(n>0)
+        {
+            r=n%10;
+            System.out.print(r);
+            n=n/10;
+        }
+    }
+}
