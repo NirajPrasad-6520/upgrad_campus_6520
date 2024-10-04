12f31b8663fa325da642cf7dac222faae1c4f9c5
Niraj <nirajprasad4y18@gmail.com>
2024-10-04 11:56:54 +0530

    first commit

--- Java/reversenumber.java
+++ Java/reversenumber.java
@@ -1,8 +1,9 @@
 import java.util.*;
 public class reversenumber {
     public static void main(String[] args) {
+    //Using While loop
         int n,r,rev=0;
-        System.out.println("Enter number:");
+        System.out.println("Enter number: ");
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
         while(n>0)
@@ -12,6 +13,19 @@ public class reversenumber {
             n=n/10;
         }
         System.out.println("Reverse number : "+rev);
+
+        System.out.println();
+
+    //Using for loop
+        int revs=0;
+        System.out.println("Enter number: ");
+        n=sc.nextInt();
+        for(;n!=0;n=n/10)
+        {
+            r=n%10;
+            revs=revs*10+r;
+        }
+        System.out.println("Reverse number: "+revs);
         sc.close();
     }
 }
