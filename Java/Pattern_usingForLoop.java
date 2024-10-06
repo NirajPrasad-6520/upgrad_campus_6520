9c29dd7f8453c5a0721201be59be7d5e99dee5b6
Niraj <nirajprasad4y18@gmail.com>
2024-10-06 20:43:26 +0530

    first commit

--- Java/Pattern_usingForLoop.java
+++ Java/Pattern_usingForLoop.java
@@ -8,8 +8,8 @@ public class Pattern_usingForLoop {
         Scanner sc=new Scanner(System.in);
         rows=sc.nextInt();
 
-        //Right Triangle Star Pattern
-        System.out.println("Right Triangle Star Pattern:");
+      //Right Triangle Star Pattern
+        System.out.println("1. Right Triangle Star Pattern:");
          for(int i=0;i < rows; i++)
          {
             for(int j=0;j<=i;j++)
@@ -18,9 +18,10 @@ public class Pattern_usingForLoop {
             }
             System.out.println();
          }
+         System.out.println("\n");
 
-        //Mirrored Right Triangle Star Pattern
-        System.out.println("Mirrored Right Triangle Star Pattern:");
+      //Mirrored Right Triangle Star Pattern
+        System.out.println("2. Mirrored Right Triangle Star Pattern:");
          for(int i=1;i <= rows; i++)
          {
              for(int j=(rows-i)*2;j>=1;j--)
@@ -29,18 +30,20 @@ public class Pattern_usingForLoop {
                 System.out.print("* ");
              System.out.println();
          }
+         System.out.println("\n");
 
-        //Downward Right Triangle Star Pattern
-        System.out.println("Downward right triangle star pattern:");
+      //Downward Right Triangle Star Pattern
+        System.out.println("3. Downward right triangle star pattern:");
          for(int i=0;i < rows; i++)
          {
              for(int j=0;j<rows-i;j++)
                 System.out.print("* ");
              System.out.println();
          }
+         System.out.println("\n");
 
-        //Mirrored downward right triangle star pattern
-        System.out.println("Mirrored downward right triangle star pattern:");
+      //Mirrored downward right triangle star pattern
+        System.out.println("4. Mirrored downward right triangle star pattern:");
          for (int i = rows; i >= 1; i--) 
                 {
                     for (int j = 1; j <= rows - i; j++)
@@ -49,9 +52,10 @@ public class Pattern_usingForLoop {
                         System.out.print("* ");
                     System.out.println();
                 }
+                System.out.println("\n");
 
-        //Pyramid Star Pattern
-        System.out.println("Pyramid Star Pattern:");
+      //Pyramid Star Pattern
+        System.out.println("5. Pyramid Star Pattern:");
          for(int i=0;i<rows;i++)
         {
             for(int j=rows-i-1;j>=1;j--)
@@ -60,9 +64,10 @@ public class Pattern_usingForLoop {
                System.out.print("* ");
             System.out.println();
         }
+        System.out.println("\n");
 
-        // Reverse Pyramid Star Pattern
-        System.out.println("Reverse Pyramid Star Pattern:");
+      // Reverse Pyramid Star Pattern
+        System.out.println("6. Reverse Pyramid Star Pattern:");
          for(int i=0;i<rows;i++)
          {
             for(int j=0;j<i;j++)
@@ -71,9 +76,10 @@ public class Pattern_usingForLoop {
                System.out.print("* ");
             System.out.println();
          }
+         System.out.println("\n");
 
-        //Diamond shape Star Pattern
-        System.out.println("Diamond shape Star Pattern:");
+      //Diamond shape Star Pattern
+        System.out.println("7. Diamond shape Star Pattern:");
          for(int i=0;i<rows;i++){
             for(int j=0;j<rows-i-1;j++)
                System.out.print(" ");
@@ -90,9 +96,10 @@ public class Pattern_usingForLoop {
                System.out.print("* ");
             System.out.println();
          }
+         System.out.println("\n");
 
-        //Number Triangle Pattern
-        System.out.println("Number Triangle Pattern:");
+      //Number Triangle Pattern
+        System.out.println("8. Number Triangle Pattern:");
          for(int i=1;i <= rows; i++)
         {
             for(int j=1;j <= i;j++)
@@ -101,9 +108,10 @@ public class Pattern_usingForLoop {
             }
             System.out.println();
         }
+        System.out.println("\n");
 
-        //Reverse Number pattern
-        System.out.println("Reverse number triangle pattern");
+      //Reverse Number pattern
+        System.out.println("9. Reverse number triangle pattern");
         for(int i=1;i<=rows;i++)
         {
          for(int j=1;j<=rows-i+1;j++)
@@ -112,11 +120,12 @@ public class Pattern_usingForLoop {
          }
          System.out.println();
         }
+        System.out.println("\n");
 
-        System.out.println("Sequence number triangle:");
-        int o=5;
+        //Floyd Triangle
+        System.out.println("10. Floyd Triangle:");
         int number=1;
-        for(int i=1;i<=o;i++)
+        for(int i=1;i<=rows;i++)
         {
          for(int j=1;j<=i;j++)
          {
@@ -125,9 +134,26 @@ public class Pattern_usingForLoop {
          }
          System.out.println();
         }
+        System.out.println("\n");
 
-        //Hollow Rectangle
-        System.out.println("Hollow Rectangle :");
+        //0,1 Triangle
+        System.out.println("11. 0-1 Triangle:");
+        for(int i=1;i<=rows;i++)
+        {
+         for(int j=1;j<=i;j++)
+         {
+            int sum=i+j;
+            if(sum % 2== 0)
+               System.out.print("1 ");
+            else
+               System.out.print("0 ");
+         }
+         System.out.println();
+        }
+        System.out.println("\n");
+
+      //Hollow Rectangle
+        System.out.println("12. Hollow Rectangle :");
         int n=6;
         int m=20;
 
@@ -144,6 +170,109 @@ public class Pattern_usingForLoop {
          }
          System.out.println();
         }
+        System.out.println("\n");
+
+        //Square Pattern 
+        System.out.println("13. Square Pattern:");
+        int p=5;
+        for(int i=1;i<=p;i++)
+        {
+         for(int j=1;j<=p;j++)
+         {
+            System.out.print("& ");
+         }
+         System.out.println();
+        }
+        System.out.println("\n");
+
+        //Butterfly pattern
+        System.out.println("14. Butterfly Pattern: ");
+        //upper half
+        for(int i=1; i<=rows; i++)
+        {
+         //1st part
+         for(int j=1; j<=i; j++)
+         {
+            System.out.print("* ");
+         }
+
+         //spaces
+         int spaces=2 * (rows-i);
+         for(int j=1; j<=spaces; j++)
+         {
+            System.out.print("  ");
+         }
+
+         //2nd part
+         for(int j=1; j<=i; j++)
+         {
+            System.out.print("* ");
+         }
+         System.out.println();
+        }
+
+        //lower half
+        for(int i=rows; i>=1; i--)
+        {
+         //1st part
+         for(int j=1; j<=i; j++)
+         {
+            System.out.print("* ");
+         }
+
+         //spaces
+         int spaces=2 * (rows-i);
+         for(int j=1; j<=spaces; j++)
+         {
+            System.out.print("  ");
+         }
+
+         //2nd part
+         for(int j=1; j<=i; j++)
+         {
+            System.out.print("* ");
+         }
+         System.out.println();
+        }
+        System.out.println("\n");
+
+        //solid Rhomus
+        System.out.println("15. Solid Rhomus:");
+        for(int i=1; i<=rows; i++)
+        {
+         //spaces
+         for(int j=1; j<=rows-i; j++)
+         {
+            System.out.print("  ");
+         }
+
+         //stars
+         for(int j=1; j<=rows; j++)
+         {
+            System.out.print("* ");
+         }
+         System.out.println();
+        }   
+        System.out.println("\n");
+
+        //Number Pyramid
+        System.out.println("16. Number Solid:");
+        for(int i=1; i<=rows; i++)
+        {
+         //spaces
+         for(int j=1; j<=rows-i; j++)
+         {
+            System.out.print("  ");
+         }
+
+         //numbers
+         for(int j=1; j<=i; j++)
+         {
+            System.out.print(i+ "   ");
+         }
+         System.out.println();
+        }   
+        System.out.println("\n");
         sc.close();
     }
 }
