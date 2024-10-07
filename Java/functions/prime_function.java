19eea1ec0672886d7aabe693262b0a0825706327
Niraj <nirajprasad4y18@gmail.com>
2024-10-07 14:45:08 +0530

    second commit

--- Java/functions/prime_function.java
+++ Java/functions/prime_function.java
@@ -8,20 +8,13 @@ public class prime_function
     {
         int count = 0;
         for(int i = 1; i <= n; i++)
-        {
             if(n % i == 0)
-            {
                 count++;
-            }
-        }
+                
         if(count == 2)
-        {
             System.out.println("Prime number.");
-        }
         else
-        {
             System.out.println("Not Prime Number.");
-        }
     }
     public static void main(String[] args) 
     {
