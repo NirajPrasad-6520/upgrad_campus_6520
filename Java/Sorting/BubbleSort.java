d43da229c600fa4c6366d10dc4eba2f63360d893
Niraj <nirajprasad4y18@gmail.com>
2024-10-21 12:13:19 +0530

    fifth commit

---
+++ Java/Sorting/BubbleSort.java
@@ -0,0 +1,35 @@
+package Sorting;
+//Bubble Sort
+// Idea: if arr[i] > arr[i+1] swap them. To place the element in their respective position, we have to do the following operation N-1 times. 
+public class BubbleSort 
+{
+    public static void printArray(int arr[])
+    {
+        for(int i = 0; i < arr.length; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) 
+    {
+        int arr[] = {7, 8, 3, 1, 2};
+
+        //Time Complexity: O(n2)
+        //bubble sort
+        for(int i = 0; i < arr.length - 1; i++)//n-1
+        {
+            for(int j = 0; j < arr.length - i -1; j++)
+            {
+                if(arr[j] > arr[j + 1])
+                {
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        printArray(arr);
+    }
+}
---
+++ Java/Sorting/SelectionSort.java
@@ -0,0 +1,37 @@
+package Sorting;
+//Selection Sort
+// Idea: The inner loop selects the minimum element in the unsorted array and places the elements in increasing order. 
+public class SelectionSort 
+{
+    public static void printArray(int arr[])
+    {
+        for(int i = 0; i < arr.length; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) 
+    {
+        int arr[] = {7, 8, 3, 1, 2};
+
+        //Time Complexity: O(n2)
+        //Selection sort
+        for(int i = 0; i < arr.length - 1; i++)
+        {
+            int smallest = i;
+            for(int j = i + 1; j < arr.length; j++)
+            {
+                if(arr[smallest] > arr[j] )
+                {
+                    smallest = j;
+                }
+            }
+            //swap
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        printArray(arr);
+    }    
+}
